431a8c77d1cc1e583ffa4470d24b647117c6b79e
Lucas Santos <lucascarvalhosantos91@gmail.com>
2026-05-17 00:18:34 -0300

    Added foreign types

--- src/preamble.zig
+++ src/preamble.zig
@@ -11,7 +11,54 @@ pub const Bool32 = enum(u32) {
     false,
     true,
 };
+pub const DeviceSize = u64;
+pub const DeviceAddress = u64;
+pub const SampleMask = u32;
+
 pub const VkRemoteAddressNV = *anyopaque;
+pub const Display = opaque {};
+pub const VisualID = c_uint;
+pub const Window = c_ulong;
+pub const RROutput = c_ulong;
+pub const wl_display = opaque {};
+pub const wl_surface = opaque {};
+pub const HINSTANCE = std.os.windows.HINSTANCE;
+pub const HWND = std.os.windows.HWND;
+pub const HMONITOR = *opaque {};
+pub const HANDLE = std.os.windows.HANDLE;
+pub const SECURITY_ATTRIBUTES = std.os.windows.SECURITY_ATTRIBUTES;
+pub const DWORD = std.os.windows.DWORD;
+pub const LPCWSTR = std.os.windows.LPCWSTR;
+pub const xcb_connection_t = opaque {};
+pub const xcb_visualid_t = u32;
+pub const xcb_window_t = u32;
+pub const zx_handle_t = u32;
+pub const _screen_context = opaque {};
+pub const _screen_window = opaque {};
+pub const _screen_buffer = opaque {};
+pub const IDirectFB = opaque {};
+pub const IDirectFBSurface = opaque {};
+pub const NvSciSyncAttrList = *opaque {};
+pub const NvSciSyncObj = *opaque {};
+pub const NvSciSyncFence = *opaque {};
+pub const NvSciBufAttrList = *opaque {};
+pub const NvSciBufObj = *opaque {};
+pub const GgpStreamDescriptor = *opaque {};
+pub const GgpFrameToken = *opaque {};
+pub const StdVideoVP9Profile = u32;
+pub const StdVideoVP9Level = u32;
+pub const ANativeWindow = opaque {};
+pub const AHardwareBuffer = opaque {};
+pub const CAMetalLayer = opaque {};
+pub const MTLDevice_id = opaque {};
+pub const MTLCommandQueue_id = opaque {};
+pub const MTLBuffer_id = opaque {};
+pub const MTLTexture_id = opaque {};
+pub const MTLSharedEvent_id = opaque {};
+pub const IOSurfaceRef = opaque {};
+pub const OHNativeWindow = opaque {};
+pub const OHBufferHandle = opaque {};
+pub const OH_NativeBuffer = opaque {};
 
 pub fn FlagsMixin(comptime Flags: type, comptime FlagBits: type) type {
     return struct {
